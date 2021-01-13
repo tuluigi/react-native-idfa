64b0c0a3c57f52564cacd2549afda3784745e19e
luigi <>
2021-01-13 12:49:14 +0800

    适配iOS14获取idfa授权异步方式

--- ios/IDFA/PTRIDFA.m
+++ ios/IDFA/PTRIDFA.m
@@ -24,11 +24,36 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(getIDFA:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    if([self isAdvertisingTrackingEnabled]) {
-        NSUUID *IDFA = [[ASIdentifierManager sharedManager] advertisingIdentifier];
-        resolve([IDFA UUIDString]);
+    /*
+     if([self isAdvertisingTrackingEnabled]) {
+     NSUUID *IDFA = [[ASIdentifierManager sharedManager] advertisingIdentifier];
+     resolve([IDFA UUIDString]);
+     } else {
+     resolve(@"");
+     }
+     */
+    if (@available(iOS 14, *)) {
+        // iOS14及以上版本需要先请求权限
+        [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+            // 获取到权限后，依然使用老方法获取idfa
+            if (status == ATTrackingManagerAuthorizationStatusAuthorized) {
+                NSString *idfa = [[ASIdentifierManager sharedManager].advertisingIdentifier UUIDString];
+                resolve(idfa);
+            } else {
+                resolve(@"");
+                //NSLog(@"请在设置-隐私-跟踪中允许App请求跟踪");
+            }
+        }];
     } else {
-        resolve(@"");
+        // iOS14以下版本依然使用老方法
+        // 判断在设置-隐私里用户是否打开了广告跟踪
+        if ([[ASIdentifierManager sharedManager] isAdvertisingTrackingEnabled]) {
+            NSString *idfa = [[ASIdentifierManager sharedManager].advertisingIdentifier UUIDString];
+            resolve(idfa);
+        } else {
+            resolve(@"");
+            NSLog(@"请在设置-隐私-广告中打开广告跟踪功能");
+        }
     }
 }
 
